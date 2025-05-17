f8a29fe1da44c537c178ccd7dad6ae0a6f5e4174
Anirudh-Reddy <anirudhreddy531@gmail.com>
2025-05-17 17:40:19 -0400

    initial

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://avk-tournaments-service.onrender.com/api',
+  socketUrl: 'https://avk-tournaments-service.onrender.com/'
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api',
-  socketUrl: 'http://localhost:3000'
+  apiUrl: 'https://avk-tournaments-service.onrender.com/api',
+  socketUrl: 'https://avk-tournaments-service.onrender.com/'
 };
