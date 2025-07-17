5cdd9f104c03e1012659ca7fe669ef506ef83f47
Anirudh-Reddy <anirudhreddy531@gmail.com>
2025-07-17 13:45:09 -0400

    updated url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://avk-tournaments-service.vercel.app/api/',
+  apiUrl: 'https://avk-tournaments-service.vercel.app/api',
   socketUrl: 'https://avk-tournaments-service.vercel.app'
 };
